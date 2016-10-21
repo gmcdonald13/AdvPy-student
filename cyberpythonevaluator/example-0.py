32b3e23b869f169cd062e52392ba9acc6d2a684e
Jason Enochs <jason@codehugger.com>
2016-10-21 05:12:17 -0400

    Added Ex7_4 and fixed a question on cyber evaluator

--- cyberpythonevaluator/example-0.py
+++ cyberpythonevaluator/example-0.py
@@ -5,11 +5,11 @@ cpe = CyberPythonEvaluator()
 challenge = cpe.get_challenge(0)
 
 print "Description", challenge.description
-print "Example Input", challenge.example_input
-print "Example Output", challenge.example_output
+#print "Example Input", challenge.example_input
+#print "Example Output", challenge.example_output
 
 def solution (input_string) :
     return input_string
 
 #print challenge.validate(solution, debug=True)
-print challenge.validate(solution, debug=True)
+print challenge.validate(solution, debug=False)
